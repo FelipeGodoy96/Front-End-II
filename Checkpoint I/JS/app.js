51ff79af2bc2c241a56b71b207fbbe38b528391c
Felipe Godoy <felipe-est@hotmail.com>
2022-05-27 00:58:29 -0300

    Checkpoint I - quase pronto

--- Checkpoint I/JS/app.js
+++ Checkpoint I/JS/app.js
@@ -1,13 +1,45 @@
-const toggleForm = document.getElementById('toggleForm')
+function toggle() {
+  const toggleForm = document.getElementById('toggleForm')
 
-toggleForm.addEventListener('click', event => {
-  const form = document.getElementById('formulario')
-  const div = document.getElementById('grid')
-  if (form.style.visibility == 'hidden') {
-    form.style.visibility = 'visible'
-  } else {
-    form.style.visibility = 'hidden'
-    // div.classList.add('g-col-11')
-    // div.classList.remove('g-col-10')
-  }
-})
+  toggleForm.addEventListener('click', event => {
+    const form = document.getElementById('formulario')
+    if (form.style.display == 'none') {
+      form.style.display = 'block'
+    } else {
+      form.style.display = 'none'
+    }
+  })
+}
+
+function add() {
+  const button = document.getElementById('submit')
+  button.addEventListener('click', event => {
+    event.preventDefault()
+    const mural = document.getElementById('mural')
+    const titulo = document.getElementById('titulo').value
+    const depoimento = document.getElementById('depoimento').value
+    const urlImagem = document.getElementById('url').value
+    let card = `<div class="g-col-12 g-col-md-4 avatar">
+    <div class="card">
+      <img
+        src="${urlImagem}"
+        class="card-img-top img-fluid"
+        
+      />
+      <div class="card-body">
+        <h5 class="card-title">${titulo}</h5>
+        <p class="card-text">
+          ${depoimento}
+        </p>
+        <a href="#" class="btn btn-primary">Ver mais</a>
+      </div>
+    </div>
+  </div>`
+    mural.insertAdjacentHTML('beforeend', card)
+  })
+}
+
+// ---  INICIALIZAÇÃO  --- //
+
+toggle()
+add()
