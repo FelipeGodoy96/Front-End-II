69d79f354c099df05c01343cee1e07455082f8b5
Felipe Godoy <felipe-est@hotmail.com>
2022-05-27 10:33:30 -0300

    Checkpoint I - validação de  dados do form

--- Checkpoint I/JS/app.js
+++ Checkpoint I/JS/app.js
@@ -11,7 +11,7 @@ function toggle() {
   })
 }
 
-function add() {
+function addDepoimento() {
   const button = document.getElementById('submit')
   button.addEventListener('click', event => {
     event.preventDefault()
@@ -19,7 +19,7 @@ function add() {
     const titulo = document.getElementById('titulo').value
     const depoimento = document.getElementById('depoimento').value
     const urlImagem = document.getElementById('url').value
-    let card = `<div class="g-col-12 g-col-md-4 avatar">
+    var card = `<div class="g-col-12 g-col-md-4 avatar">
     <div class="card">
       <img
         src="${urlImagem}"
@@ -30,16 +30,53 @@ function add() {
         <h5 class="card-title">${titulo}</h5>
         <p class="card-text">
           ${depoimento}
-        </p>
-        <a href="#" class="btn btn-primary">Ver mais</a>
+        </p>      
       </div>
     </div>
   </div>`
-    mural.insertAdjacentHTML('beforeend', card)
+    const tituloErro = document.getElementById('tituloErro')
+    const depoimentoErro = document.getElementById('depoimentoErro')
+    const urlErro = document.getElementById('urlErro')
+    function validateTitulo() {
+      if (titulo == null || titulo === '') {
+        tituloErro.textContent = 'Campo obrigatório!'
+      } else {
+        return true
+      }
+    }
+    function validateDepoimento() {
+      if (depoimento == null || depoimento === '') {
+        depoimentoErro.textContent = 'Campo obrigatório!'
+      } else {
+        return true
+      }
+    }
+    function validateURL() {
+      if (urlImagem == null || urlImagem === '') {
+        urlErro.textContent = 'Campo obrigatório!'
+      } else {
+        return true
+      }
+    }
+
+    validateTitulo()
+    validateDepoimento()
+    validateURL()
+
+    if (!validateTitulo() && !validateDepoimento() && !validateURL()) {
+      const submitErro = document.getElementById('submitErro')
+      submitErro.textContent = ''
+    } else {
+      tituloErro.textContent = ''
+      depoimentoErro.textContent = ''
+      urlErro.textContent = ''
+      submitErro.textContent = ''
+      mural.insertAdjacentHTML('beforeend', card)
+    }
   })
 }
 
 // ---  INICIALIZAÇÃO  --- //
 
 toggle()
-add()
+addDepoimento()
