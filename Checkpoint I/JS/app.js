bb7c2769e457978ea80b2b1efc43f1ad947c7081
Felipe Godoy <felipe-est@hotmail.com>
2022-05-27 11:04:42 -0300

    Checkpoint I - validação de dados completa, enfeitando

--- Checkpoint I/JS/app.js
+++ Checkpoint I/JS/app.js
@@ -1,6 +1,5 @@
 function toggle() {
   const toggleForm = document.getElementById('toggleForm')
-
   toggleForm.addEventListener('click', event => {
     const form = document.getElementById('formulario')
     if (form.style.display == 'none') {
@@ -39,33 +38,48 @@ function addDepoimento() {
     const urlErro = document.getElementById('urlErro')
     function validateTitulo() {
       if (titulo == null || titulo === '') {
-        tituloErro.textContent = 'Campo obrigatório!'
+        tituloErro.textContent = 'Campo obrigatório.'
       } else {
+        tituloErro.textContent = ''
         return true
       }
     }
     function validateDepoimento() {
       if (depoimento == null || depoimento === '') {
-        depoimentoErro.textContent = 'Campo obrigatório!'
+        depoimentoErro.textContent = 'Campo obrigatório.'
       } else {
+        depoimentoErro.textContent = ''
         return true
       }
     }
     function validateURL() {
       if (urlImagem == null || urlImagem === '') {
-        urlErro.textContent = 'Campo obrigatório!'
+        urlErro.textContent = 'Campo obrigatório.'
+      } else if (!isUrl(urlImagem)) {
+        urlErro.textContent = 'Dado inserido não corresponde a uma URL'
       } else {
+        urlErro.textContent = ''
         return true
       }
+    }
+
+    function isUrl(url) {
+      return /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/.test(
+        url
+      )
     }
 
     validateTitulo()
     validateDepoimento()
     validateURL()
 
-    if (!validateTitulo() && !validateDepoimento() && !validateURL()) {
+    if (!validateTitulo()) {
       const submitErro = document.getElementById('submitErro')
-      submitErro.textContent = ''
+      submitErro.textContent = 'Um ou mais campos estão inválidos!'
+    } else if (!validateDepoimento()) {
+      submitErro.textContent = 'Um ou mais campos estão inválidos!'
+    } else if (!validateURL()) {
+      submitErro.textContent = 'Um ou mais campos estão inválidos!'
     } else {
       tituloErro.textContent = ''
       depoimentoErro.textContent = ''
