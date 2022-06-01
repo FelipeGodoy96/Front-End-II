4f823deaed2e8813cb3ead491f1bf7d6fe63a9a2
Felipe Godoy <felipe-est@hotmail.com>
2022-06-01 15:01:23 -0300

    Aula 14

---
+++ aula14/JS/script.js
@@ -0,0 +1,13 @@
+const inpKey = document.getElementById('inpKey')
+const inpValue = document.getElementById('inpValue')
+const btnInsert = document.getElementById('btnInsert')
+const isOutput = document.getElementById('isOutput')
+
+btnInsert.addEventListener('click', e => {
+  e.preventDefault()
+  const key = inpKey.value
+  const value = inpValue.value
+
+  if (key && value) {
+  }
+})
