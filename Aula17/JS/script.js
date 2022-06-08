511dcec9f7d444cd23436d10dcd396044c0f2552
Felipe Godoy <felipe-est@hotmail.com>
2022-06-08 07:46:25 -0300

    Aula 17 - mesa

---
+++ Aula17/JS/script.js
@@ -0,0 +1,46 @@
+// async function fetchAPI() {
+//   await fetch('https://jsonplaceholder.typicode.com/todos').then(response => {
+//     response.json().then(data => {
+//       const lista = document.getElementById('lista')
+//       data.map(element => {
+//         const li = document.createElement('li')
+//         li.setAttribute('id', element.id)
+//         li.innerHTML = `<h1> ${element.title} </h1>`
+//         lista.appendChild(li)
+
+//         if (element.completed) {
+//           li.innerHTML += `<h2 style='color: green'> Concluido </h2>`
+//         } else {
+//           li.innerHTML += `<h2 style='color: red'> Não concluido </h2>`
+//         }
+//       })
+//     })
+//   })
+// }
+
+async function fetchAPI() {
+  fetch('https://pokeapi.co/api/v2/pokemon').then(response => {
+    response.json().then(data => {
+      const lista = document.getElementById('lista')
+      data.results.map(element => {
+        const li = document.createElement('li')
+        const img = document.createElement('img')
+
+        const imgData = async function () {
+          await fetch(`${element.url}`).then(response2 => {
+            response2.json().then(data2 => {
+              return data2.sprites.front_default
+            })
+          })
+        }
+        img.setAttribute('src', imgData)
+
+        li.innerHTML = `<h1>${element.name}</h1>`
+        lista.appendChild(li)
+        lista.appendChild(img)
+      })
+    })
+  })
+}
+
+fetchAPI()
--- aula14/JS/script.js
+++ aula14/JS/script.js
@@ -2,6 +2,7 @@ const inpKey = document.getElementById('inpKey')
 const inpValue = document.getElementById('inpValue')
 const btnInsert = document.getElementById('btnInsert')
 const isOutput = document.getElementById('isOutput')
+const clearBtn = document.getElementById('clearBtn')
 
 btnInsert.addEventListener('click', e => {
   e.preventDefault()
@@ -9,5 +10,11 @@ btnInsert.addEventListener('click', e => {
   const value = inpValue.value
 
   if (key && value) {
+    localStorage.setItem(key, value)
+    location.reload()
   }
+})
+
+clearBtn.addEventListener('click', () => {
+  localStorage.clear()
 })
