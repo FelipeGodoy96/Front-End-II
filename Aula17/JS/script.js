458513ac9fcea0739ed111c8e334587f229e526b
Felipe Godoy <felipe-est@hotmail.com>
2022-06-11 12:51:12 -0300

    Aula 17 - API pokemon + imagens resolvida

--- Aula17/JS/script.js
+++ Aula17/JS/script.js
@@ -1,23 +1,3 @@
-// async function fetchAPI() {
-//   await fetch('https://jsonplaceholder.typicode.com/todos').then(response => {
-//     response.json().then(data => {
-//       const lista = document.getElementById('lista')
-//       data.map(element => {
-//         const li = document.createElement('li')
-//         li.setAttribute('id', element.id)
-//         li.innerHTML = `<h1> ${element.title} </h1>`
-//         lista.appendChild(li)
-
-//         if (element.completed) {
-//           li.innerHTML += `<h2 style='color: green'> Concluido </h2>`
-//         } else {
-//           li.innerHTML += `<h2 style='color: red'> Não concluido </h2>`
-//         }
-//       })
-//     })
-//   })
-// }
-
 async function fetchAPI() {
   fetch('https://pokeapi.co/api/v2/pokemon').then(response => {
     response.json().then(data => {
@@ -25,7 +5,6 @@ async function fetchAPI() {
       data.results.map(element => {
         const li = document.createElement('li')
         const img = document.createElement('img')
-
         function secondFetch() {
           fetch(`${element.url}`).then(response2 => {
             response2.json().then(data2 => {
@@ -34,7 +13,6 @@ async function fetchAPI() {
           })
         }
         secondFetch()
-
         li.innerHTML = `<h1>${element.name}</h1>`
         lista.appendChild(li)
         lista.appendChild(img)
@@ -42,5 +20,3 @@ async function fetchAPI() {
     })
   })
 }
-
-fetchAPI()
